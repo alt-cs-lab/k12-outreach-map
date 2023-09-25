dc6d1b4426e7521612ffb2fa10b450aa89e1f109
Nathan H. Bean <nhbean@ksu.edu>
2023-09-25 16:15:28 +0000

    Added with/height support

--- src/Pages/Map.cshtml.cs
+++ src/Pages/Map.cshtml.cs
@@ -36,12 +36,16 @@ public class MapModel : PageModel
     /// <param name="districts">The school districts to mark</param>
     /// <param name="fill">The color to highlight the specified districts with</param>
     /// <returns>A customized SVG file to serve</returns>
-    public async Task<IActionResult> OnGet(string districts, string fill="#512888")
+    public async Task<IActionResult> OnGet(string districts, int? width, int? height, string fill="#512888")
     {
         // Use AngleSharp to load the SVG string into a manipulable document 
         AngleSharp.IConfiguration config = Configuration.Default;
         IBrowsingContext context = BrowsingContext.New(config);
         IDocument doc = await context.OpenAsync(req => req.Content(SvgText));
+
+        // Grab the top-level SVG element, if it exists
+        IElement? svgElement = doc.QuerySelector("svg");
+        if(svgElement == null) return StatusCode(500);
         
         // Convert fill color to a hex code string if necessary
         if(fill.All("0123456789abcdefABCDEF".Contains))
@@ -61,9 +65,33 @@ public class MapModel : PageModel
             }
         }
 
+        // Modify the image size
+        if(width != null || height != null )
+        {
+            if(width != null && height != null) {
+                // Apply the specified width and height 
+                // Note, this can distort the image if it does not conform to the aspect ratio!
+                svgElement.SetAttribute("width", width.ToString());
+                svgElement.SetAttribute("height", height.ToString());
+            }
+            else if(width != null) {
+                // Apply the specified width, and calculate the corresponding height from the aspect ratio
+                const double aspectRatio = 400.0/700.0;
+                int newHeight = (int)Math.Floor(width.Value * aspectRatio);
+                svgElement.SetAttribute("width", width.ToString());
+                svgElement.SetAttribute("height", newHeight.ToString());
+            }
+            else if(height != null){
+                // Apply the specified height, and calculate the corresponding width from the aspect ratio
+                const double aspectRatio = 700.0/400.0;
+                int newWidth = (int)Math.Floor(height.Value * aspectRatio);
+                svgElement.SetAttribute("width", newWidth.ToString());
+                svgElement.SetAttribute("height", height.ToString());
+            }
+        }
+
+
         // Serve the modified SVG
-        IElement? svgElement = doc.QuerySelector("svg");
-        if(svgElement == null) return StatusCode(500);
-        else return Content(svgElement.OuterHtml, "image/svg+xml");
+        return Content(svgElement.OuterHtml, "image/svg+xml");
     }
 }
